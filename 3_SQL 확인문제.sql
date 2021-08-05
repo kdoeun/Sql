70f81099d5cee341156579cec7774d8cca069c64
khjyo2001 <khjyo2001@gmail.com>
2021-08-05 13:04:31 +0900

    2021/08/03 SQL 확인문제

--- 3_SQL 확인문제.sql
+++ 3_SQL 확인문제.sql
@@ -1,29 +1,31 @@
-#2-1
-khjyo2001CREATE TABLE `USER2` (
-	`uid`		VARCHAR(10) PRIMARY KEY,
-	`name`   VARCHAR(10),
-	`hp`		CHAR(13),
-	`age` 	INT 
+#날짜 : 2021/08/03
+#이름 : 김도은
+#내용 : SQL 확인문제
+
+#실습하기 3-1
+CREATE TABLE `tbl_member` (
+	`memberID`			CHAR(8) PRIMARY KEY,
+	`memberName`		CHAR(5) NOT NULL,
+	`memberAddress`	CHAR(20)
 );
 
-INSERT INTO `user2` VALUES('A101khjyo2001', '김춘추', '010-1234-1111', 23);
-INSERT INTO `user2` VALUES('A102', '김유신', '010-1234-2222', 25);
-INSERT INTO `user2` VALUES('A103', '장보고', '010-1234-3333', 31);
-
-INSERT INTO `user2` VALUES('A104', '김춘추', '010-1234-1111', 23);
-
-
-#2-2
-CREATE TABLE `USER3` (
-	`uid`		VARCHAR(10) PRIMARY KEY,
-	`name` 	VARCHAR(10),
-	`hp`		CHAR(13) UNIQUE,
-	`age`		INT 
+CREATE TABLE `tbl_product` (
+	`productName`	CHAR(4) PRIMARY KEY,
+	`cost`			INT NOT NULL,
+	`makeDate`		DATE,
+	`company`		CHAR(5),
+	`amount`	   	INT NOT NULL
 );
 
-INSERT INTO `user3` VALUES('A101', '김춘추', '010-1234-1111', 23);
-INSERT INTO `user3` VALUES('A102', '김유신', '010-1234-2222', 25);
-INSERT INTO `user3` VALUES('A103', '장보고', '010-1234-3333', 31);
-INSERT INTO `user3` VALUES('A104', '김춘추', '010-1234-1111', 23);
+#실습하기 3-2
+INSERT INTO `tbl_member` VALUES ('jang', '장보고', '완도군');
+INSERT INTO `tbl_member` VALUES ('kang', '강감찬', '서울시');
+INSERT INTO `tbl_member` (`memberID`, `memberName`) VALUES ('kim', '김유신');
+INSERT INTO `tbl_member` VALUES ('lee', '이순신', '서울시');
 
+INSERT INTO `tbl_product` VALUES ('냉장고',  800, '2021-05-17', 'LG', 1);
+INSERT INTO `tbl_product` VALUES ('노트북', 1500, '2021-05-17', '삼성', 5);
+INSERT INTO `tbl_product` VALUES ('컴퓨터', 1000, '2021-05-17', 'LG', 10);
 
+#실습하기 3-3
+SELECT * FROM `tbl_member` WHERE `memberAddress` = '서울시';
